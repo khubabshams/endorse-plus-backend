76bd571ce6fa03191b3f30edc820c09e205ad746
Khubab <khubab220@gmail.com>
2023-05-14 08:47:29 +0000

    Add heroku app link to allowed hosts

--- wiserbuys_backend/settings.py
+++ wiserbuys_backend/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'wiserbuys-backend.herokuapp.com',
+]
 
 
 # Application definition
