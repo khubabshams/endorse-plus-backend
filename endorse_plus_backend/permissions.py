eacf16b3854439e0fb06a28ae69daa3c039cfce0
Khubab <khubab220@gmail.com>
2023-05-24 21:17:53 +0000

    Add condition in permission for models with just profile field not owner

--- endorse_plus_backend/permissions.py
+++ endorse_plus_backend/permissions.py
@@ -5,4 +5,5 @@ class IsOwnerOrReadonly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.owner == request.user if hasattr(obj, 'owner') \
+            else obj.profile.owner == request.user
