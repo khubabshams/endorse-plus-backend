9d1e855de5de3e3f534ec6556fbd9d928ab1f395
Khubab <khubab220@gmail.com>
2023-05-24 19:31:08 +0000

    Add profileserializer and view, and is owner permission

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from endorse_plus_backend.permissions import IsOwnerOrReadonly
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListCreateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Profile.objects.all()
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadonly]
+    queryset = Profile.objects.all()
