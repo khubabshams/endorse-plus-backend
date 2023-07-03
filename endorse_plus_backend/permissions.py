3fbfaeab14f017c0940fc4994611625eaf569055
kshamse <khubab220@gmail.com>
2023-07-03 02:47:33 +0100

    Change request detail view permission

--- endorse_plus_backend/permissions.py
+++ endorse_plus_backend/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 
 class IsOwnerOrReadonly(permissions.BasePermission):
@@ -14,3 +15,12 @@ class IsAdminOrReadOnly(permissions.IsAdminUser):
     def has_permission(self, request, view):
         is_admin = super(IsAdminOrReadOnly, self).has_permission(request, view)
         return request.method in permissions.SAFE_METHODS or is_admin
+
+
+class IsOwnerOrReceiver(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS or \
+                request.user in [obj.profile.owner, obj.receiver.owner]:
+            return True
+        raise PermissionDenied({'detail':
+                                'You don not have permission to access'})
