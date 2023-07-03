936c3eba94e40b75ab1c1e6c9e04549a94d30d4f
kshamse <khubab220@gmail.com>
2023-07-03 02:26:58 +0100

    Change request detail view permission

--- endorse_plus_backend/permissions.py
+++ endorse_plus_backend/permissions.py
@@ -14,10 +14,3 @@ class IsAdminOrReadOnly(permissions.IsAdminUser):
     def has_permission(self, request, view):
         is_admin = super(IsAdminOrReadOnly, self).has_permission(request, view)
         return request.method in permissions.SAFE_METHODS or is_admin
-
-
-class IsOwnerOrReceiver(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user in [obj.profile.owner, obj.receiver.owner]
