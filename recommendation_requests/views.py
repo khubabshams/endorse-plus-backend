e335e2d8bbd37147bbffe343b8bd1dca317436b0
kshamse <khubab220@gmail.com>
2023-07-03 03:12:14 +0100

    Change request add filtering

--- recommendation_requests/views.py
+++ recommendation_requests/views.py
@@ -2,13 +2,13 @@ from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from endorse_plus_backend.permissions import IsOwnerOrReadonly
 from .models import Request
+from django.db.models import Q
 from .serializers import RequestSerializer, RequestSeenSerializer
 
 
 class RequestList(generics.ListCreateAPIView):
     serializer_class = RequestSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Request.objects.all()
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
@@ -26,19 +26,34 @@ class RequestList(generics.ListCreateAPIView):
     ]
     ordering_fields = [
         'created_at',
+        'seen',
     ]
 
     def perform_create(self, serializer):
         serializer.save(profile=self.request.user.profile)
 
+    def get_queryset(self):
+        user_profile = self.request.user.profile
+        return Request.objects.\
+            filter(Q(profile=user_profile) | Q(receiver=user_profile))
+
 
 class RequestDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RequestSerializer
     permission_classes = [IsOwnerOrReadonly]
-    queryset = Request.objects.all()
+
+    def get_queryset(self):
+        user_profile = self.request.user.profile
+        return Request.objects.\
+            filter(Q(profile=user_profile) | Q(receiver=user_profile))
 
 
 class RequestSeen(generics.RetrieveUpdateAPIView):
     serializer_class = RequestSeenSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Request.objects.all()
+
+    def get_queryset(self):
+        user_profile = self.request.user.profile
+        return Request.objects.\
+            filter(Q(profile=user_profile) | Q(receiver=user_profile))
